8868faaea30646d82669f5b47256e5c5fbac764a
imaderama <s160419075@student.ubaya.ac.id>
2021-10-14 12:15:53 +0800

    init

---
+++ app/src/main/java/id/ac/ubaya/informatika/todoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
