4dee1920abc85b81a69e3733cd84cfcd4b471133
imaderama <s160419075@student.ubaya.ac.id>
2021-10-21 15:47:12 +0800

    edit MainActivity.kt

--- app/src/main/java/id/ac/ubaya/informatika/todoapp/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/todoapp/view/MainActivity.kt
@@ -4,28 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import id.ac.ubaya.informatika.todoapp.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navControllerC: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        //actionbar
-//        val actionbar = supportActionBar
-//        //set actionbar title
-//        actionbar!!.title = "New Activity"
-//        //set back button
-//        actionbar.setDisplayHomeAsUpEnabled(true)
-//
-//        navControllerC = Navigation.findNavController(this, R.id.hostFragment)
-//        NavigationUI.setupActionBarWithNavController(this, navControllerC)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navControllerC, null)
+        return NavigationUI.navigateUp(navController, null)
     }
 }
